202df24bfe115351017c50a68ecfc87f362ab014
austinxduong <axduong2@gmail.com>
2021-05-02 00:48:05 -0700

    added PokemonList

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,11 @@
 import { Component } from 'react';
 import './App.css';
+import header from './header';
 import React from 'react';
+import request from 'superagent';
+import PokemonSearch from '../pokemon/PokemonSearch';
+import PokemonList from '../pokemon/PokemonList';
+import Paging from './Paging';
 
 const POKEMON_API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
@@ -8,13 +13,87 @@ const POKEMON_API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
 class App extends Component {
 
+  state = {
+    pokemon: [],
+    search: '',
+    page: 1
+
+  }
+
+  componentDidMount() {
+    this.fetchPokemon();
+  }
+
+  async fetchPokemon() {
+    const { search, page } = this.state;
+    
+
+
+    const response = await request
+      .get(POKEMON_API_URL)
+      .query({ pokemon: search })
+      .query({ page: page });
+
+    
+    this.setState({
+      pokemon: response.body.results,
+    });
+  }
+
+  handleSearch = ({ search }) => {
+    this.setState(
+      { search: search, page: 1 },
+      () => this.fetchPokemon()
+    );
+  }
+
+  
+  handlePrevPage = () => {
+    this.setState(
+      { page: Math.max(this.state.page - 1, 1) },
+      () => this.fetchPokemon()
+    );
+  }
+
+
+  handleNextPage = () => {
+    this.setState(
+      { page: Math.max(this.state.page + 1) },
+      () => this.fetchPokemon()
+    );
+  }
+
 
   render() {
+
+    const { pokemon, page } = this.state;
+
+
     return (
       <div className="App">
+
+        <header />
+
+        <section className="search-options">
+          <PokemonSearch onSearch={this.handleSearch} />
+
+          <Paging
+            page={page}
+            onPrev={this.handlePrevPage}
+            onNext={this.handleNextPage}
+          />
+
+        </section>
   
+        <main>
+
+          <PokemonList pokemon={pokemon} />
+
+
+        </main>
       
-      
+
+
       </div>
     );
   }
